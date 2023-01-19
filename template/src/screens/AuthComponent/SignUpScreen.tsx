e5f90d7521a51205d7867fea92eeea6a979ad843
Anh Le <anh.lehoang@saigontechnology.com>
2023-01-19 11:31:34 +0700

    chore: update tsx file

--- template/src/screens/AuthComponent/SignUpScreen.tsx
+++ template/src/screens/AuthComponent/SignUpScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ScreenContainer from '../../components/ScreenContainer'
 
-export const SignUpScreen = (props: IProps) => {
+export const SignUpScreen = () => {
   return <ScreenContainer children={<></>} style={{}} />
 }
