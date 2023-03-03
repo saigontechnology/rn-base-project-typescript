f884826c25604889a70cbce241e966973e36987e
Anh Le <anh.lehoang@saigontechnology.com>
2023-03-03 13:32:37 +0700

    chore: resolve conflict

--- template/src/screens/AuthComponent/SignUpScreen.tsx
+++ template/src/screens/AuthComponent/SignUpScreen.tsx
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import ScreenContainer from '../../components/ScreenContainer'
-
-export const SignUpScreen = (props: IProps) => {
-  return <ScreenContainer children={<></>} style={{}} />
-=======
 import {NativeStackScreenProps} from '@react-navigation/native-stack'
 import React from 'react'
 import ScreenContainer from '../../components/ScreenContainer'
@@ -16,5 +9,4 @@ type Props = NativeStackScreenProps<AppStackParamList, RouteKey.SignUpScreen>
 export const SignUpScreen: React.FC<Props> = props => {
   const {navigation, route} = props
   return <ScreenContainer children={<></>} />
->>>>>>> 583e5afa6c63bfc601c34c2ffdf9cbde8cf758cf
 }
