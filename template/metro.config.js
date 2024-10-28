a16adc8f54496c46f164baa16cd227bb7def582b
danhmaigts <danh.mai@saigontechnology.com>
2024-10-28 15:16:57 +0700

    feat: upgrade to react native 0.76

--- template/metro.config.js
+++ template/metro.config.js
@@ -1,5 +1,5 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config')
-
+const {wrapWithReanimatedMetroConfig} = require('react-native-reanimated/metro-config')
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
@@ -8,4 +8,4 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config')
  */
 const config = {}
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config)
+module.exports = wrapWithReanimatedMetroConfig(mergeConfig(getDefaultConfig(__dirname), config))
