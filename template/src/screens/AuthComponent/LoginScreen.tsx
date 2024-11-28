4ed7997a9afce34c4905bf1b66280e3fc3a1161e
Loc NguyenThien <loc.nguyent@saigontechnology.com>
2024-11-28 11:10:31 +0700

    fix: more feedbacks after using

--- template/src/screens/AuthComponent/LoginScreen.tsx
+++ template/src/screens/AuthComponent/LoginScreen.tsx
@@ -1,9 +1,9 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack'
-import React, {PropsWithChildren} from 'react'
+import React from 'react'
 import {ScreenContainer} from '../../components'
 import RouteKey from '../../navigation/RouteKey'
 import {AppStackParamList} from '../../navigation/types'
 
-type Props = NativeStackScreenProps<AppStackParamList, RouteKey.LoginScreen> & PropsWithChildren
+type Props = NativeStackScreenProps<AppStackParamList, RouteKey.LoginScreen>
 
-export const LoginScreen: React.FC<Props> = props => <ScreenContainer>{props.children}</ScreenContainer>
+export const LoginScreen: React.FC<Props> = () => <ScreenContainer />
--- template/src/screens/SplashScreen.tsx
+++ template/src/screens/SplashScreen.tsx
@@ -5,7 +5,7 @@ import * as Progress from 'react-native-progress'
 import {ScreenContainer} from '../components'
 import configs from '../constants/configs'
 import {appActions} from '../store/reducers/app'
-import {useAppDispatch} from '../store/store'
+import {useDispatch} from 'react-redux'
 import {colors, deviceWidth, metrics, responsiveHeight} from '../themes'
 
 const codePushOptions = {
@@ -20,7 +20,7 @@ const STATUS = {
 }
 
 const SplashScreen = () => {
-  const dispatch = useAppDispatch()
+  const dispatch = useDispatch()
   const [updatePercent, setUpdatePercent] = useState(0)
   const [statusText, setStatusText] = useState('')
 
